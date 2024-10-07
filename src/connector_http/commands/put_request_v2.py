76cde0922cfb3bc39f379f94e0ff24a6d47fe345
suylim <suyashlimaye@gmail.com>
2024-10-07 12:23:22 +0530

    Update put_request_v2.py

--- src/connector_http/commands/put_request_v2.py
+++ src/connector_http/commands/put_request_v2.py
@@ -1,26 +1,50 @@
+
 from typing import Any
 
 import requests  # type: ignore
+from spiffworkflow_connector_command.command_interface import CommandResultDictV1
 from spiffworkflow_connector_command.command_interface import ConnectorCommand
-from spiffworkflow_connector_command.command_interface import ConnectorProxyResponseDict
+import xmltodict
+import json
+from requests.auth import HTTPDigestAuth
 
-from connector_http.http_request_base import HttpRequestBase
-
-
-class PutRequestV2(ConnectorCommand, HttpRequestBase):
-    def __init__(
-        self,
+class GetRequest(ConnectorCommand):
+    def __init__(self,
         url: str,
         headers: dict[str, str] | None = None,
         data: dict[str, str] | None = None,
         basic_auth_username: str | None = None,
         basic_auth_password: str | None = None,
+        
     ):
-        HttpRequestBase.__init__(
-            self, url=url, headers=headers, basic_auth_username=basic_auth_username, basic_auth_password=basic_auth_password
-        )
-
+        self.url = url
+        self.headers = headers or {}
+        self.params = params or {}
+        self.basic_auth_username = basic_auth_username
+        self.basic_auth_password = basic_auth_password
         self.data = data or {}
 
-    def execute(self, _config: Any, _task_data: dict) -> ConnectorProxyResponseDict:
-        return self.run_request(requests.put)
+    # backend
+    #   spiffworkflow-proxy
+    #     GetRequest returns CommandResultDictV1
+    def execute(self, _config: Any, _task_data: Any) -> CommandResultDictV1:
+        auth = None
+        if self.basic_auth_username is not None and self.basic_auth_password is not None:
+            # auth = (self.basic_auth_username, self.basic_auth_password)
+            auth=HTTPDigestAuth(self.basic_auth_username,self.basic_auth_password)
+
+        try:
+            response = requests.request("PUT",self.url, self.params, headers=self.headers, auth=auth, data=self.data,timeout=300,verify=False)
+
+            return {
+                "response": response.text,
+                "status": response.status_code,
+                "mimetype": "application/json",
+            }
+        except Exception as e:
+            return {
+                "response": f'{"error": {e}}',
+                "status": 500,
+                "mimetype": "application/json",
+            }
+
